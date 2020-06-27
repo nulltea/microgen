28a5a48b3b34731980999f11b2174796e8a1ab15
Timothy <tyalugin@outlook.com>
2020-06-28 01:58:17 +0300

    feat (codegen) subdirectory generation edited

--- usecase/init.go
+++ usecase/init.go
@@ -3,7 +3,6 @@ package usecase
 import (
 	"fmt"
 	"os"
-	"path/filepath"
 
 	"github.com/timoth-y/microgen/core/meta"
 )
@@ -21,23 +20,24 @@ func (s operations) Init(param meta.OperationParam) error {
 		return err
 	}
 
+	fmt.Fprintf(os.Stdout, "Microservice structure generated successfully")
 	return nil
 }
 
 func (s operations) generateEntitiesLayer() error {
-	if err := os.MkdirAll(filepath.Dir("core"), 0755); err != nil {
+	if err := os.MkdirAll("core", os.ModeDir); err != nil {
 		return fmt.Errorf("unable to create CORE dir: " + err.Error())
 	}
 
-	if err := os.MkdirAll(filepath.Dir("core/model"), 0755); err != nil {
+	if err := os.MkdirAll("core/model", os.ModePerm); err != nil {
 		return fmt.Errorf("unable to create CORE/MODEL dir: " + err.Error())
 	}
 
-	if err := os.MkdirAll(filepath.Dir("core/service"), 0755); err != nil {
+	if err := os.MkdirAll("core/service", os.ModePerm); err != nil {
 		return fmt.Errorf("unable to create CORE/SERVICE dir: " + err.Error())
 	}
 
-	if err := os.MkdirAll(filepath.Dir("core/repo"), 0755); err != nil {
+	if err := os.MkdirAll("core/repo", os.ModePerm); err != nil {
 		return fmt.Errorf("unable to create CORE/REPO dir: " + err.Error())
 	}
 
@@ -45,15 +45,15 @@ func (s operations) generateEntitiesLayer() error {
 }
 
 func (s operations) generateUseCaseLayer() error {
-	if err := os.MkdirAll(filepath.Dir("usecase"), 0755); err != nil {
+	if err := os.MkdirAll("usecase", os.ModePerm); err != nil {
 		return fmt.Errorf("unable to create USECASE dir: " + err.Error())
 	}
 
-	if err := os.MkdirAll(filepath.Dir("usecase/storage"), 0755); err != nil {
+	if err := os.MkdirAll("usecase/storage", os.ModePerm); err != nil {
 		return fmt.Errorf("unable to create USECASE/STORAGE dir: " + err.Error())
 	}
 
-	if err := os.MkdirAll(filepath.Dir("usecase/business"), 0755); err != nil {
+	if err := os.MkdirAll("usecase/business", os.ModePerm); err != nil {
 		return fmt.Errorf("unable to create USECASE/BUSINESS dir: " + err.Error())
 	}
 
@@ -61,15 +61,15 @@ func (s operations) generateUseCaseLayer() error {
 }
 
 func (s operations) generateGatewayLayer() error {
-	if err := os.MkdirAll(filepath.Dir("api"), 0755); err != nil {
+	if err := os.MkdirAll("api", os.ModePerm); err != nil {
 		return fmt.Errorf("unable to create API dir: " + err.Error())
 	}
 
-	if err := os.MkdirAll(filepath.Dir("api/rest"), 0755); err != nil {
+	if err := os.MkdirAll("api/rest", os.ModePerm); err != nil {
 		return fmt.Errorf("unable to create API/REST dir: " + err.Error())
 	}
 
-	if err := os.MkdirAll(filepath.Dir("api/graphQL"), 0755); err != nil {
+	if err := os.MkdirAll("api/graphQL", os.ModePerm); err != nil {
 		return fmt.Errorf("unable to create API/GRAPHQL dir: " + err.Error())
 	}
 
